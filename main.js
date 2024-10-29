355bc1b381f41ee16d4d4f5bfa6052f2103b60e4
Jomar <jomarbolasco98.com>
2024-10-29 15:53:25 +0800

    adding modal to projects

--- main.js
+++ main.js
@@ -49,3 +49,15 @@ function typeWords() {
 
 // Start the typing effect
 setTimeout(typeWords, delayBetweenWords);
+
+function openModal(imgElement) {
+  var modalImage = document.getElementById("modalImage");
+  modalImage.src = imgElement.src; // Set the image source in the modal
+}
+
+function openDemo() {
+  const websiteURL = "https://addbot.netlify.app/";
+  document.querySelector("#demoModal iframe").src = websiteURL;
+  const modal = new bootstrap.Modal(document.getElementById("demoModal"));
+  modal.show();
+}
